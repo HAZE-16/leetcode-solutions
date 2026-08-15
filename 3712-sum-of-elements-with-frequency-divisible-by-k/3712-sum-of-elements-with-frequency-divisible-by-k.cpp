c397491671005466a48237b7ad560f559be0f4c9
Rohit Sharma <rohithsarma738@gmail.com>
2026-08-15 23:59:33 +0530

    Time: 0 ms (100%), Space: 26.1 MB (78.31%) - LeetHub

---
+++ 3712-sum-of-elements-with-frequency-divisible-by-k/3712-sum-of-elements-with-frequency-divisible-by-k.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int sumDivisibleByK(vector<int>& nums, int k) {
+        int n = nums.size();
+        unordered_map<int,int> mp;
+        for(int num : nums)
+        {
+            mp[num]++;
+        }
+        int sum = 0;
+        for(auto it : mp)
+        {
+            if(it.second % k == 0)
+            {
+                sum += it.first * it.second;
+            }
+        }
+        return sum;
+    }
+};
