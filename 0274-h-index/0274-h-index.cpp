e5151fb7e4789320d5eb4540933c912b9d2f4ca6
Rohit Sharma <rohithsarma738@gmail.com>
2026-08-20 01:17:11 +0530

    Time: 0 ms (100%), Space: 12.1 MB (98.99%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -2,23 +2,15 @@ class Solution {
 public:
     int hIndex(vector<int>& citations) {
         int n = citations.size();
-        int count =0;
-        for(int i =0 ;i<=n;i++)
+        sort(citations.begin(),citations.end());
+        for(int i =0;i<citations.size();i++)
         {
-            int ans = 0;
-            for(int j = 0;j<n;j++)
+            int h = n -i;
+            if(citations[i] >= h)
             {
-                if(citations[j] >= i)
-                {
-                    ans++;
-                    
-                }
-                if(ans >= i)
-                {
-                count = max(count,i);
-                }
+                return h;
             }
         }
-        return count;
+        return 0;
     }
 };
