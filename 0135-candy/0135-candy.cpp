b12f3dd87704ded191187645df5f61aedf7d63d6
Rohit Sharma <rohithsarma738@gmail.com>
2026-08-23 03:59:18 +0530

    Time: 0 ms (100%), Space: 73.7 MB (8.09%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -1,32 +1,26 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
+
         int n = ratings.size();
+        vector<int> lcnt(n,1);
+        vector<int> rcnt(n,1);
 
-        vector<int> candy(n, 1);
-
-        // Left → Right
-        for(int i = 1; i < n; i++)
-        {
-            if(ratings[i] > ratings[i - 1])
-            {
-                candy[i] = candy[i - 1] + 1;
+        for(int i=1;i<n;i++){
+            if(ratings[i] > ratings[i-1]){
+                lcnt[i] = lcnt[i-1]+1;
             }
         }
 
-        // Right → Left
-        for(int i = n - 2; i >= 0; i--)
-        {
-            if(ratings[i] > ratings[i + 1])
-            {
-                candy[i] = max(candy[i], candy[i + 1] + 1);
+        for(int i=n-2;i>=0;i--){
+            if(ratings[i] > ratings[i+1]){
+                rcnt[i] = rcnt[i+1]+1;
             }
         }
 
-        int ans = 0;
-        for(int x : candy)
-        {
-            ans += x;
+        int ans=0;
+        for(int i=0;i<n;i++){
+            ans+= max(lcnt[i],rcnt[i]);
         }
 
         return ans;
