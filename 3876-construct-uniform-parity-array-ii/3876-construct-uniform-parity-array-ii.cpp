08469071450b6fe13c6f6903e7770c159374967c
Rohit Sharma <rohithsarma738@gmail.com>
2026-09-03 15:20:45 +0530

    Time: 3 ms (72.51%), Space: 165.8 MB (62.83%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-        sort(nums1.begin(),nums1.end());
-
+        int mini = *min_element(nums1.begin(),nums1.end());
         for(int i = 0;i<nums1.size();i++)
         {
         
-             if(nums1[0] % 2 == 0)
+             if(mini % 2 == 0)
             {
                 if(nums1[i] % 2 != 0)
                 {
